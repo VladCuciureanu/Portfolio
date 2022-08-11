c52d6b417a4d8136416efd11290afb8a76b559d0
VladCuciureanu <dev@vladinski.md>
2022-08-11 13:08:13 +0300

    :mag: Updated OG to new design

--- src/utils/og.ts
+++ src/utils/og.ts
@@ -16,13 +16,13 @@ export const createOgImage = ({
 
     // TITLE
     // Karla font in accent color
-    `l_text:Karla_72_bold:${e(title)},co_rgb:ffe4e6,c_fit,w_1400,h_240`,
+    `l_text:Karla_72_bold:${e(title)},co_rgb:ededed,c_fit,w_1400,h_240`,
     // Positioning
     `fl_layer_apply,g_south_west,x_100,y_180`,
 
     // DESCRIPTION
     // Karla font, but smaller
-    `l_text:Karla_48:${e(description)},co_rgb:ffe4e680,c_fit,w_1400`,
+    `l_text:Karla_48:${e(description)},co_rgb:a0a0a0,c_fit,w_1400`,
     // Positioning
     `fl_layer_apply,g_south_west,x_100,y_100`,
 
@@ -30,7 +30,7 @@ export const createOgImage = ({
     // Asset
     `l_avatar`,
     // Transformations
-    `c_thumb,g_face,r_max,w_380,h_380,q_100`,
+    `r_max,w_380,h_380,c_scale,q_100`,
     // Positioning
     `fl_layer_apply,w_140,g_north_west,x_100,y_100`,
 
