7cf9ae9f93d8d7e83a59d9ef3b1e125b2b063f77
VladCuciureanu <dev@vladinski.md>
2022-08-11 12:28:03 +0300

    :truck: Moving wrongly placed component

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/ui/shared/layout"
 import SharedStyles from "@/ui/shared/styles"
 import styled from "styled-components"
-import ProjectCard from "./project-card"
+import ProjectCard from "../../ui/projects/project-card"
 
 export default function Projects() {
   return (
