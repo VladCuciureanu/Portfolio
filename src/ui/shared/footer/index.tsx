4fc6911b4b472b527e4e976e26f03128efd5c2bc
VladCuciureanu <dev@vladinski.md>
2022-09-03 00:43:50 +0300

    :sparkles: Added Vercel note in footer

--- src/ui/shared/footer/index.tsx
+++ src/ui/shared/footer/index.tsx
@@ -1,10 +1,20 @@
 import { AnimatePresence } from "framer-motion"
+import styled from "styled-components"
 import Dock from "./dock"
 
 export default function Footer() {
   return (
     <AnimatePresence>
       <Dock />
+      <Note>Powered by ▲Vercel</Note>
     </AnimatePresence>
   )
 }
+
+const Note = styled.div`
+  color: var(--colors-gray9);
+  font-size: smaller;
+  position: absolute;
+  bottom: 0px;
+  left: calc(50% - 80px);
+`
