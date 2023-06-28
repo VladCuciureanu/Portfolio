80fccecb2351b6a64057f68cf8b88d5844a4403f
Vlad Cuciureanu <vlad.cuciureanu@pm.me>
2023-06-28 18:38:37 +0300

    fix: SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "TODO SEO",
+  title: "Vlad Cuciureanu | Portfolio",
+  // TODO: SEO
 };
 
 export default function RootLayout({
