f06dd83c3f0ffab07c2ff2d7c753f41cf3d28788
Vlad Cuciureanu <vlad.cuciureanu@pm.me>
2023-06-29 11:03:33 +0300

    refactor: Projects

--- src/app/(main)/projects/page.tsx
+++ src/app/(main)/projects/page.tsx
@@ -1,6 +1,6 @@
-import { Project } from "@/types/project";
 import styles from "./page.module.scss";
 import ProjectsTable from "@/features/Projects/Table";
+import Projects from "@/constants/projects";
 
 export default function ProjectsPage() {
   return (
@@ -8,78 +8,7 @@ export default function ProjectsPage() {
       <h1 className={styles.Title}>Projects</h1>
       <p className={styles.Job}>It&apos;s not much but it&apos;s honest work</p>
       <div className={styles.Divider} />
-      <ProjectsTable data={TableData} />
+      <ProjectsTable data={Projects} />
     </>
   );
 }
-
-const TableData: Project[] = [
-  {
-    name: "Lyra",
-    description:
-      "Self‑hosted music streaming service that allows you to take your high‑fidelity music collection anywhere.",
-    year: "2021",
-    href: "https://github.com/faith-software/Lyra",
-  },
-  {
-    name: "Hymn",
-    description:
-      "Open-source, non-profit, free-access, christian music tabs repository.",
-    year: "2021",
-    href: "https://github.com/faith-software/Hymn",
-  },
-  {
-    name: "Portfolio",
-    description:
-      "My personal website, serving as a digital garden and portfolio, showcasing my thoughts, experiences, and projects.",
-    year: "2022",
-    href: "https://github.com/VladCuciureanu/Portfolio",
-  },
-  {
-    name: "Toothpick",
-    description:
-      "Bluetooth extension for Raycast. Provides shortcuts for quick Bluetooth management.",
-    year: "2022",
-    href: "https://github.com/VladCuciureanu/Toothpick",
-  },
-  {
-    name: "CheckIt",
-    description: "A minimalist, immersive, light checklist application.",
-    year: "2021",
-    href: "https://github.com/VladCuciureanu/CheckIt",
-  },
-  {
-    name: "Garlic Bread",
-    description:
-      "Is your sense of humor similar to that of an neanderthal? If so, check out this tasty Discord bot!",
-    year: "2020",
-    href: "https://github.com/VladCuciureanu/GarlicBread",
-  },
-  {
-    name: "Degree",
-    description: "REST-based audio streaming showcased in React and .NET Core.",
-    year: "2021",
-    href: "https://github.com/VladCuciureanu/Degree",
-  },
-  {
-    name: "SwaggerUIWatch",
-    description:
-      "Hot-reloading Swagger UI server. See live updates to your OpenAPI specifications!",
-    year: "2022",
-    href: "https://github.com/faith-software/SwaggerUIWatch",
-  },
-  {
-    name: "UnitBuddy",
-    description:
-      "Browser extension that converts all measuring units to your preferred system of units.",
-    year: "2022",
-    href: "https://github.com/faith-software/UnitBuddy",
-  },
-  {
-    name: "Khora",
-    description:
-      "Open-source all-in-one portable synthesizer, sampler and controller.",
-    year: "2019",
-    href: "https://github.com/faith-software/Khora",
-  },
-];
---
+++ src/constants/projects.ts
@@ -0,0 +1,70 @@
+import { Project } from "@/types/project";
+
+const Projects: Project[] = [
+  {
+    name: "Lyra",
+    description: "Self‑hosted, high-fidelity music streaming service",
+    year: "2021",
+    href: "https://github.com/faith-software/Lyra",
+  },
+  {
+    name: "Hymn",
+    description:
+      "Open-source, non-profit, free-access, christian music tabs repository",
+    year: "2021",
+    href: "https://github.com/faith-software/Hymn",
+  },
+  {
+    name: "Portfolio",
+    description:
+      "My personal website, serving as a digital garden and portfolio",
+    year: "2022",
+    href: "https://github.com/VladCuciureanu/Portfolio",
+  },
+  {
+    name: "Toothpick",
+    description:
+      "Bluetooth extension for Raycast that provides shortcuts for quick Bluetooth management",
+    year: "2022",
+    href: "https://github.com/VladCuciureanu/Toothpick",
+  },
+  {
+    name: "CheckIt",
+    description: "An immersive minimalist checklist application",
+    year: "2021",
+    href: "https://github.com/VladCuciureanu/CheckIt",
+  },
+  {
+    name: "Garlic Bread",
+    description: "Discord bot for server administration and management",
+    year: "2020",
+    href: "https://github.com/VladCuciureanu/GarlicBread",
+  },
+  {
+    name: "Degree",
+    description: "REST-based audio streaming showcased in React and .NET Core",
+    year: "2021",
+    href: "https://github.com/VladCuciureanu/Degree",
+  },
+  {
+    name: "SwaggerUIWatch",
+    description: "Hot-reloading Swagger UI server",
+    year: "2022",
+    href: "https://github.com/faith-software/SwaggerUIWatch",
+  },
+  {
+    name: "UnitBuddy",
+    description:
+      "Browser extension that converts all measuring units to your preferred system of units",
+    year: "2022",
+    href: "https://github.com/faith-software/UnitBuddy",
+  },
+  {
+    name: "Khora",
+    description: "All-in-one portable synthesizer, sampler and controller",
+    year: "2019",
+    href: "https://github.com/faith-software/Khora",
+  },
+];
+
+export default Projects;
