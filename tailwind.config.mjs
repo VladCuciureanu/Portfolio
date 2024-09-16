b6b4fe9e8591c45306641186a598e99d5deed15d
Vlad Cuciureanu <vlad@cuciureanu.net>
2024-09-16 18:24:18 +0300

    feat: Refined color palette

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--color-background)",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        tertiary: "var(--color-tertiary)",
+        underline: "var(--color-underline)",
+      },
+    },
   },
   plugins: [],
 };
